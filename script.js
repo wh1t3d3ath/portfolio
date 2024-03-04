b4230cc73a62d2428d2c0c1fd90461b0a694c586
Vaggelis kavouras <vaggeliskavouras6@gmail.com>
2024-03-04 16:37:39 +0200

    Dark modified

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Dark Mode
+
+let btn = document.getElementById("btn");
+let btnText = document.getElementById("btnText");
+let btnIcon = document.getElementById("btnIcon")
+
+
+btn.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains(dark-theme)){
+    btnIcon.src = "<./assets/sun-solid.svg>";
+    btnText.innerHTML = "Light";
+  }
+  else{
+    btnIcon.src = "./aseets/moon-solid.svg"
+    btnText.innerHTML = "Dark";
+  }
+}
