45bcca3fc0e1da631f29460521063dac4b8a3a38
Vaggelis kavouras <vaggeliskavouras6@gmail.com>
2024-03-07 19:30:25 +0200

    Splash Screen

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-  // Hamburger Menu
-  function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+   //Splash Screen
 
-  // Dark / Light Mode
-  const btn = document.getElementById("modeToogle")
-  const btn2 = document.getElementById("modeToogle2")
-  const themeIcons = document.querySelectorAll(".icon");
-  let isSystemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+   window.addEventListener('load', function () {
+    setTimeout(function () {
+        document.getElementById('splash-screen').style.display = 'none';
+    }, 2000);  // 3000ms = 3s
+});
 
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-      isSystemDarkMode = e.matches;
-      setTheme();
-  });
 
-  if(localStorage.getItem("theme") === "dark"){
+// Hamburger Menu
+function toggleMenu() {
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
+menu.classList.toggle("open");
+icon.classList.toggle("open");
+}
+
+// Dark / Light Mode
+const btn = document.getElementById("modeToogle")
+const btn2 = document.getElementById("modeToogle2")
+const themeIcons = document.querySelectorAll(".icon");
+let isSystemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  isSystemDarkMode = e.matches;
+  setTheme();
+});
+
+if(localStorage.getItem("theme") === "dark"){
+setDarkMode();
+};
+
+btn.addEventListener("click" , function(){
+localStorage.setItem("themeChangedManually", "true");
+setTheme();
+})
+btn2.addEventListener("click" , function(){
+localStorage.setItem("themeChangedManually", "true");
+setTheme();
+})
+
+function setTheme(){
+let currentTheme = document.body.getAttribute("theme")
+let themeChangedManually = localStorage.getItem("themeChangedManually");
+
+if(themeChangedManually !== "true"){
+  if(isSystemDarkMode){
     setDarkMode();
-  };
+  } else {
+    setLightMode();
+  }
+} else if(currentTheme === "dark"){
+  setLightMode();
+} else {
+  setDarkMode();
+}
+};
 
-  btn.addEventListener("click" , function(){
-    localStorage.setItem("themeChangedManually", "true");
-    setTheme();
-  })
-  btn2.addEventListener("click" , function(){
-    localStorage.setItem("themeChangedManually", "true");
-    setTheme();
-  })
+function setDarkMode(){
+document.body.setAttribute("theme" , "dark");
+localStorage.setItem("theme" , "dark");
+themeIcons.forEach((icon) => (
+  icon.src = icon.getAttribute("src-dark")
+))
+};
 
-  function setTheme(){
-    let currentTheme = document.body.getAttribute("theme")
-    let themeChangedManually = localStorage.getItem("themeChangedManually");
-
-    if(themeChangedManually !== "true"){
-      if(isSystemDarkMode){
-        setDarkMode();
-      } else {
-        setLightMode();
-      }
-    } else if(currentTheme === "dark"){
-      setLightMode();
-    } else {
-      setDarkMode();
-    }
-  };
-
-  function setDarkMode(){
-    document.body.setAttribute("theme" , "dark");
-    localStorage.setItem("theme" , "dark");
-    themeIcons.forEach((icon) => (
-      icon.src = icon.getAttribute("src-dark")
-    ))
-  };
-
-  function setLightMode(){
-    document.body.removeAttribute("theme");
-    localStorage.setItem("theme" , "light");
-    themeIcons.forEach((icon) => (
-      icon.src = icon.getAttribute("src-light")
-    ))
-  };
+function setLightMode(){
+document.body.removeAttribute("theme");
+localStorage.setItem("theme" , "light");
+themeIcons.forEach((icon) => (
+  icon.src = icon.getAttribute("src-light")
+))
+};
